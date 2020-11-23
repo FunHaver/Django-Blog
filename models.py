39aa647bf8e99580b3d3186d8958d8436ad226a0
FunHaver <conortsullivan@gmail.com>
2020-11-23 02:21:35 +0000

    created first database models and database migration

--- models.py
+++ models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class Author(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+
+class Post(models.Model):
+    post_title = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+    author = models.ForeignKey(Author, on_delete=models.PROTECT)
+    post_body = models.TextField()
+
