ff9f2d82bb749c903078b3002df7155718f44dec
FunHaver <conortsullivan@gmail.com>
2020-11-25 21:51:14 +0000

    added html templates and two views for listing and viewing posts

--- urls.py
+++ urls.py
@@ -2,6 +2,10 @@ from django.urls import path
 
 from . import views
 
+app_name = 'blog'
 urlpatterns = [
+    # ex: /blog/
     path('', views.index, name='index'),
+    # ex: /blog/title
+    path('<post_title_url>', views.detail, name='detail'),
 ]
--- views.py
+++ views.py
@@ -1,6 +1,21 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+
+from .models import Post
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the blog index")
+    latest_post_list = Post.objects.order_by('-pub_date')[:5]
+    context = {
+        'latest_post_list': latest_post_list,
+    }
+
+    return render(request, 'blog/index.html', context)
+
+def detail(request, post_title_url):
+    post = get_object_or_404(Post, post_title=post_title_url)
+    
+    context = {
+        'post': post,
+    }
+    
+    return render(request, 'blog/detail.html', context)
